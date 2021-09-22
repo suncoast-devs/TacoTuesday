afc4a6eb70be983fcbd48e26ed01a36de1dfc723
Gavin Stark <gavin@gstark.com>
2021-09-22 11:18:07 -0400

    Makes NewRestaurant page function and introduces some basic routing

--- ClientApp/src/main.tsx
+++ ClientApp/src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import './index.scss'
 import { App } from './App'
 import { QueryClient, QueryClientProvider } from 'react-query'
@@ -8,9 +10,11 @@ const queryClient = new QueryClient()
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
